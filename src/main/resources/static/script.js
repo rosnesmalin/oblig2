7fff2f9acaca1fb5351bc6d8c7b0c736e45b6ef0
rosnesmalin <malin.rosnes@gmail.com>
2023-03-15 11:04:12 +0100

    Endret alt til jQuery, og gjor litt småendringer

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,62 +1,64 @@
-src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
 
 
 function kjop() {
 //objektet opprettes
     const Billett = {
-        film: document.getElementById('film').value,
-        antall: document.getElementById('antall').value,
-        fornavn: document.getElementById('fornavn').value,
-        etternavn: document.getElementById('etternavn').value,
-        telefonnr: document.getElementById('telefonnr').value,
-        epost: document.getElementById('epost').value
-    }
-    //sjekker om alle input-feltene er fyllt ut
-    let error = false
-    if (Billett.antall == "" ) {
-        document.getElementById('ant').innerHTML="Du må oppgi antall biletter";
-        error = true
-    } else {
-        document.getElementById('ant').innerHTML="";
-    }
-    if (Billett.fornavn == "") {
-        document.getElementById('for').innerHTML="Du må oppgi navn";
-        error = true
-    } else {
-        document.getElementById('for').innerHTML="";
-    }
-    if (Billett.etternavn == "") {
-        document.getElementById('ette').innerHTML="Du må oppgi etternavn";
-        error = true
-    } else {
-        document.getElementById('ette').innerHTML="";
-    }
-    if (Billett.telefonnr == "") {
-        document.getElementById('tlf').innerHTML="Du må oppgi telefonnr";
-        error = true
-    }else {
-        document.getElementById('tlf').innerHTML="";
-    }
-    if (Billett.epost == "") {
-        document.getElementById('epo').innerHTML="Du må oppgi epost-adresse";
-        error = true
-    }else {
-        document.getElementById('epo').innerHTML="";
+        film: $("#film").val(),
+        antall: $("#antall").val(),
+        fornavn: $("#fornavn").val(),
+        etternavn: $("#etternavn").val(),
+        telefonnr: $("#telefonnr").val(),
+        epost: $("#epost").val()
     }
 
-    if(!error) {
-    //kaller på metoden på serveren som legger objektet inn i et array
-    $.post("/kjop", Billett, function() {
-        hentRegister();
-    });
+    //sjekker om alle input-feltene er fyllt ut
+    let error = false
+    if (Billett.antall === "" ) {
+        $("#ant").html("Du må oppgi antall biletter");
+        error = true
+    } else {
+        $("#ant").html("");
+    }
+    if (Billett.fornavn === "") {
+        $("#for").html("Du må oppgi navn");
+        error = true
+    } else {
+        $("#for").html("");
+    }
+    if (Billett.etternavn === "") {
+        $("#ette").html("Du må oppgi etternavn");
+        error = true
+    } else {
+        $("#ette").html("");
+    }
+    if (Billett.telefonnr === "") {
+        $("#tlf").html("Du må oppgi telefonnr");
+        error = true
+    }else {
+        $("#tlf").html("");
+    }
+    if (Billett.epost === "") {
+        $("#epo").html("Du må oppgi epost-adresse");
+        error = true
+    }else {
+        $("#epo").html("");
+    }
+
+    if(error == false) {
+        console.log(Billett)
+        //kaller på metoden på serveren som legger objektet inn i et array
+        $.post("/kjop", Billett, function () {
+            hentRegister();
+        });
         // sørger for at feil-meldingene går bort dersom feltene fylles ut
-        document.getElementById('fil').innerHTML = "";
-        document.getElementById('ant').innerHTML = "";
-        document.getElementById('for').innerHTML = "";
-        document.getElementById('ette').innerHTML = "";
-        document.getElementById('tlf').innerHTML = "";
-        document.getElementById('epo').innerHTML = "";
-}}
+        $("#fil").html("");
+        $("#ant").html("");
+        $("#for").html("");
+        $("#ette").html("");
+        $("#tlf").html("");
+        $("#epo").html("");
+    }
+}
     //kaller på metoden på serveren som henter arrayet
 function hentRegister() {
     $.post("/hentRegister", function (data) {
@@ -66,11 +68,11 @@ function hentRegister() {
     //legger arrayet som er hentet ut inn i en tabell. Kobler sammen arrayet og tabellen.
 function formaterData(Billett) {
     // tømmer input-feltene
-    document.getElementById('antall').value = null;
-    document.getElementById('fornavn').value = null;
-    document.getElementById('etternavn').value = null;
-    document.getElementById('telefonnr').value = null;
-    document.getElementById('epost').value = null;
+    $("#antall").val("");
+    $("#fornavn").val("");
+    $("#etternavn").val("");
+    $("#telefonnr").val("");
+    $("#epost").val("");
 
     let ut =
         "<table class='table table-striped table-bordered'>"
@@ -90,7 +92,7 @@ function formaterData(Billett) {
             + "<tr>";
     }
     ut += "</table>";
-    document.getElementById('registerListet').innerHTML = ut;
+    $("#registerListet").html(ut);
 }
 
 // nullstiller arrayet
